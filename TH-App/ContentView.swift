1dbee8ca664e264b6e0481b8bf4f654dd7692b97
mrzogy <magbolmrzogy@gmail.com>
2023-06-28 17:52:12 +0300

    Initial Commit

---
+++ TH-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TH-App
+//
+//  Created by 3bood on 10/12/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TH-App/TH_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TH_AppApp.swift
+//  TH-App
+//
+//  Created by 3bood on 10/12/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TH_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
